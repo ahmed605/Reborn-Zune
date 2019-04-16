98c0e377a25709bc2c930069e79dfdc2ef741057
JUNDA YIN <yinjunda.cool@gmail.com>
2019-04-16 11:02:27 -0400

    Add Unit Test

---
+++ MusicLibraryServiceTest/DatabaseTest.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicLibraryEFCoreModel;
+using Windows.Storage;
+
+namespace MusicLibraryServiceTest
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void DatabaseInitialize()
+        {
+            using(var _context = new MusicLibraryDbContext())
+            {
+                _context.Database.Migrate();
+                Assert.IsTrue(_context.Database.CanConnect());
+            }
+        }
+
+        [TestMethod]
+        public void DatabaseInsert()
+        {
+            using (var _context = new MusicLibraryDbContext())
+            {
+                Assert.IsTrue(_context.Database.CanConnect());
+            }
+        }
+    }
+}
