14da8d312353f0fac0f5d553a7c9ceaba33c0a0e
JUNDA YIN <yinjunda.cool@gmail.com>
2019-04-16 14:00:48 -0400

    Add Library Layer Unit test

--- MusicLibraryServiceTest/DatabaseTest.cs
+++ MusicLibraryServiceTest/DatabaseTest.cs
@@ -1,8 +1,11 @@
 ﻿
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MusicLibraryEFCoreModel;
+using MusicLibraryService;
 using Windows.Storage;
 
 namespace MusicLibraryServiceTest
@@ -13,19 +16,48 @@ namespace MusicLibraryServiceTest
         [TestMethod]
         public void DatabaseInitialize()
         {
-            using(var _context = new MusicLibraryDbContext())
+            try
             {
-                _context.Database.Migrate();
-                Assert.IsTrue(_context.Database.CanConnect());
+                DataBaseService.Initialize();
+            }
+            catch(Exception e)
+            {
+                Assert.Fail();
+            }
+            
+        }
+
+        [TestMethod]
+        public async Task DatabseInsertAndFetch()
+        {
+            try
+            {
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Test.mp3"));
+                await DataBaseService.Add(file);
+                var result = DataBaseService.FetchAll();
+                Assert.IsTrue(result.musics.Count >= 1);
+                Assert.IsTrue(result.albums.Count >= 1);
+                Assert.IsTrue(result.artists.Count >= 1);
+                Assert.IsTrue(result.thumbnails.Count >= 1);
+            }
+            catch(Exception e)
+            {
+                Assert.Fail();
             }
         }
 
         [TestMethod]
-        public void DatabaseInsert()
+        public async Task DataBaseUpdate()
         {
-            using (var _context = new MusicLibraryDbContext())
+            try
             {
-                Assert.IsTrue(_context.Database.CanConnect());
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Test.mp3"));
+                await DataBaseService.Add(file);
+                await DataBaseService.Update(file);
+            }
+            catch(Exception e)
+            {
+                Assert.Fail();
             }
         }
     }
