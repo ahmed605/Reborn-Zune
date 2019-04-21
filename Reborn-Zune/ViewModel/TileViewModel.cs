988260cf44cc56bf4ea1e3ee7299b31cf2b2d99a
JUNDA YIN <yinjunda.cool@gmail.com>
2019-04-21 18:23:30 -0400

    Remove unused code

--- Reborn-Zune/ViewModel/TileViewModel.cs
+++ Reborn-Zune/ViewModel/TileViewModel.cs
@@ -194,41 +194,6 @@ namespace Reborn_Zune.ViewModel
             Tiles = a;
             GC.Collect();
         }
-
-        private async Task<WriteableBitmap> GrayScaleBitmap(WriteableBitmap thumbnail)
-        {
-            byte[] srcPixels = new byte[4 * thumbnail.PixelWidth * thumbnail.PixelHeight];
-            using (Stream pixelStream = thumbnail.PixelBuffer.AsStream())
-            {
-                await pixelStream.ReadAsync(srcPixels, 0, srcPixels.Length);
-            }
-            WriteableBitmap dstBitmap =
-               new WriteableBitmap(thumbnail.PixelWidth, thumbnail.PixelHeight);
-            byte[] dstPixels = new byte[4 * dstBitmap.PixelWidth * dstBitmap.PixelHeight];
-            for (int i = 0; i < srcPixels.Length; i += 4)
-            {
-                double b = (double)srcPixels[i] / 255.0;
-                double g = (double)srcPixels[i + 1] / 255.0;
-                double r = (double)srcPixels[i + 2] / 255.0;
-
-                byte a = srcPixels[i + 3];
-
-                double e = (0.21 * r + 0.71 * g + 0.07 * b) * 255;
-                byte f = Convert.ToByte(e);
-
-                dstPixels[i] = f;
-                dstPixels[i + 1] = f;
-                dstPixels[i + 2] = f;
-                dstPixels[i + 3] = a;
-            }
-            // Move the pixels into the destination bitmap
-            using (Stream pixelStream = dstBitmap.PixelBuffer.AsStream())
-            {
-                await pixelStream.WriteAsync(dstPixels, 0, dstPixels.Length);
-            }
-            dstBitmap.Invalidate();
-            return dstBitmap;
-        }
         #endregion
     }
 }
