67b08186160d905710cbe57bf4da34ad3302d04d
JUNDA YIN <yinjunda.cool@gmail.com>
2019-09-18 14:45:27 -0400

    Update for Auto Signing

---
+++ appcenter-pre-build.ps1
@@ -0,0 +1,1 @@
+New-SelfSignedCertificate -Type Custom -Subject "CN=691BCA65-0225-4D09-BA12-DFA680F370B8" -KeyUsage DigitalSignature -FriendlyName "PackageAutoSign" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
