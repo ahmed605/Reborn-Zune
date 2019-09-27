8c12f69e31baef4a9b4870599869ff4a17500671
JUNDA YIN <yinjunda.cool@gmail.com>
2019-09-27 17:54:11 -0400

    Changed

--- Reborn-Zune/App.xaml.cs
+++ Reborn-Zune/App.xaml.cs
@@ -20,8 +20,6 @@ namespace Reborn_Zune
     /// </summary>
     sealed partial class App : Application
     {
-       
-
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -31,7 +29,7 @@ namespace Reborn_Zune
             
             this.InitializeComponent();
             this.Construct();
-            AppCenter.Start("c5fcab4c-7057-489f-b350-f74dd185fdad", typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("ffbf2e68-b5cf-44b3-9bd8-88990d3e6307", typeof(Analytics), typeof(Crashes));
 
             ServiceLocator.SetInstance(new MusicLibraryService());
 
@@ -50,46 +48,57 @@ namespace Reborn_Zune
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
+            if (e.PreviousExecutionState != ApplicationExecutionState.Running)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                bool loadState = (e.PreviousExecutionState == ApplicationExecutionState.Terminated);
+                ExtendSplash extendedSplash = new ExtendSplash(e.SplashScreen, loadState);
+                Window.Current.Content = extendedSplash;
             }
 
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(500, 300));
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
 
-                // using Windows.UI.ViewManagement;
-                ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(500,300));
-                var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
-                coreTitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.Activate();
 
-                
+            //Frame rootFrame = Window.Current.Content as Frame;
 
-                Window.Current.Activate();
-            }
+            //// Do not repeat app initialization when the Window already has content,
+            //// just ensure that the window is active
+            //if (rootFrame == null)
+            //{
+            //    // Create a Frame to act as the navigation context and navigate to the first page
+            //    rootFrame = new Frame();
+
+            //    rootFrame.NavigationFailed += OnNavigationFailed;
+
+            //    if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            //    {
+            //        //TODO: Load state from previously suspended application
+            //    }
+
+            //    // Place the frame in the current Window
+            //    Window.Current.Content = rootFrame;
+            //}
+
+            //if (e.PrelaunchActivated == false)
+            //{
+            //    if (rootFrame.Content == null)
+            //    {
+            //        // When the navigation stack isn't restored navigate to the first page,
+            //        // configuring the new page by passing required information as a navigation
+            //        // parameter
+            //        rootFrame.Navigate(typeof(SplashScreen), e.Arguments);
+            //    }
+            //    // Ensure the current window is active
+
+            //    // using Windows.UI.ViewManagement;
+            
+
+
+
+            //    Window.Current.Activate();
+            //}
         }
 
         /// <summary>
---
+++ Reborn-Zune/ExtendSplash.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Microsoft.Toolkit.Uwp.UI.Animations;
+using Reborn_Zune_Common.Services;
+using Reborn_Zune_MusicLibraryService;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using Microsoft.Toolkit.Uwp.Helpers;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Reborn_Zune
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExtendSplash : Page
+    {
+        internal Rect splashImageRect; // Rect to store splash screen image coordinates.
+        private SplashScreen splash; // Variable to hold the splash screen object.
+        internal bool dismissed = false; // Variable to track splash screen dismissal status.
+        internal Frame rootFrame;
+
+        public ExtendSplash(SplashScreen splashscreen, bool loadState)
+        {
+            this.InitializeComponent();
+            TitleBarSetting();
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
+
+            splash = splashscreen;
+            if (splash != null)
+            {
+                // Register an event handler to be executed when the splash screen has been dismissed.
+                splash.Dismissed += new TypedEventHandler<SplashScreen, Object>(DismissedEventHandler);
+
+
+                DrawGradient();
+                // Retrieve the window coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+
+                // If applicable, include a method for positioning a progress control.
+                //PositionRing();
+            }
+
+            // Create a Frame to act as the navigation context
+            rootFrame = new Frame();
+
+        }
+
+        private static void TitleBarSetting()
+        {
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.InactiveBackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.InactiveForegroundColor = Colors.White;
+            titleBar.ButtonBackgroundColor = "#00000000".ToColor();
+            titleBar.ButtonForegroundColor = Colors.DarkGray;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveForegroundColor = Colors.DarkGray;
+            titleBar.ButtonHoverBackgroundColor = Colors.Transparent;
+            titleBar.ButtonHoverForegroundColor = Colors.Black;
+            titleBar.ButtonPressedBackgroundColor = "#f5f5f5".ToColor();
+            titleBar.ButtonPressedForegroundColor = Colors.Black;
+        }
+
+        private void ExtendSplash_Completed(object sender, EventArgs e)
+        {
+            rootFrame.Navigate(typeof(MainPage));
+            // Place the frame in the current Window
+            Window.Current.Content = rootFrame;
+        }
+
+        private async void DrawGradient()
+        {
+            await gradientColors.Fade(1.0f, 415, 20, EasingType.Linear, Windows.UI.Xaml.Media.Animation.EasingMode.EaseInOut).StartAsync();
+            await extendedSplashImage.Fade(1.0f, 415, 20, EasingType.Linear, Windows.UI.Xaml.Media.Animation.EasingMode.EaseInOut).StartAsync();
+        }
+
+        void DismissedEventHandler(SplashScreen sender, object e)
+        {
+            dismissed = true;
+
+            (ServiceLocator.GetInstance("MusicLibraryService") as MusicLibraryService).Completed += ExtendSplash_Completed;
+        }
+
+        private void ExtendedSplash_OnResize(object sender, WindowSizeChangedEventArgs e)
+        {
+            // Safely update the extended splash screen image coordinates. This function will be executed when a user resizes the window.
+            if (splash != null)
+            {
+                // Update the coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+
+                // If applicable, include a method for positioning a progress control.
+                // PositionRing();
+            }
+        }
+
+        void PositionImage()
+        {
+            extendedSplashImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
+            extendedSplashImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
+            extendedSplashImage.Height = splashImageRect.Height;
+            extendedSplashImage.Width = splashImageRect.Width;
+        }
+
+        //void PositionRing()
+        //{
+        //    splashProgressRing.SetValue(Canvas.LeftProperty, splashImageRect.X + (splashImageRect.Width * 0.5) - (splashProgressRing.Width * 0.5));
+        //    splashProgressRing.SetValue(Canvas.TopProperty, (splashImageRect.Y + splashImageRect.Height + splashImageRect.Height * 0.1));
+        //}
+
+    }
+}
